05b8c6fa2fae259901c9f3b4cb397a4b119e6266
Sebastian Ferreyra <sebasferr@icloud.com>
2026-05-20 16:09:09 -0300

    config: change base path to /devs/mgames/

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/devs/utnestrategicos/',
+  base: '/devs/mgames/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
